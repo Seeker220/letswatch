0db1ad0e73a3aad126a678c7b884cbbc675169f7
Asmit Manab Karmakar <asmit36362@gmail.com>
2024-12-03 11:10:09 +0530

    Reapply "Made basic PWA App"
    This reverts commit 1d43e8abc042cef4cf319d48511ad65e2a8ed23f.

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'letswatch-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/seasons.html',
+    '/episodes.html',
+    '/manifest.json',
+    '/api/fetchEpisodes.js',
+    '/api/fetchSeasons.js',
+    '/api/searchMovies.js',
+    '/api/searchSeries.js'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) =>
+            Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            )
+        )
+    );
+});
